8bd1688771ec120463320e08d1fd0f6a5db2596a
colorcalendar <45058315+colorcalendar@users.noreply.github.com>
2021-06-17 15:27:26 -0700

    Update display-color-date.js

--- display-color-date.js
+++ display-color-date.js
@@ -8,29 +8,14 @@ function displayColorDate() {
         document.getElementById("color-date").textContent = "Clock is set too far in the past";
     }
 
-    // Check is the clock is set too far in the future
-    // Most clocks won't go 99 years ahead because of 2038 problem
-    else if (timeRightNow > newYearEpochDate[99]) {
+    // Check if the clock is set too far in the future
+    // Many clocks won't go more than 17 years ahead because of 2038 problem
+    else if (timeRightNow > newYearEpochDate[17]) {
         document.getElementById("color-date").textContent = "Clock is set too far in the future";
     } 
 
-   // Else display the current Color Calendar date 
+   // Display the current Color Calendar date 
     else {document.getElementById("color-date").textContent = "Year " + colorYear + ", " + colorMonth +
             " " + colorMonthDay + ", " + colorDay;
     }
 }
-
-
-// Set size of font based on screen size
-if (window.innerHeight < window.innerWidth){
-
-// If height is less than width, set for height
-var d = document.getElementById('color-date');
-d.style.fontSize = window.innerHeight/9+'px';
-}else{
-
-// else set for width
-var d = document.getElementById('color-date');
-d.style.fontSize = window.innerWidth/15+'px';
-};
-
